2f4014e82e68e151416e1a15ed624ad7cd0324f2
Steve Kinney <hello@stevekinney.net>
2014-11-04 16:50:34 -0700

    Change prompts

--- logger.rb
+++ logger.rb
@@ -1,7 +1,7 @@
 require './lib/redis-config'
 
 begin
-  puts "You're listening on Slacker."
+  puts "You're logging on Slacker."
   puts "Press Ctrl-C at any time to exit.\n"
   $redis.subscribe(:community) do |on|
     on.message do |channel, msg|
