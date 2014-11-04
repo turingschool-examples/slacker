70915d47152d0f4e393e0c5da7e486582efedfec
Steve Kinney <hello@stevekinney.net>
2014-11-04 15:20:09 -0700

    Add some CLI flare

--- listener.rb
+++ listener.rb
@@ -1,8 +1,14 @@
 require './lib/redis-config'
 
-$redis.subscribe(:community) do |on|
-  on.message do |channel, msg|
-    data = JSON.parse(msg)
-    puts "##{channel} - [#{data['user']}]: #{data['msg']}"
+begin
+  puts "You're listening on Slacker."
+  puts "Press Ctrl-C at any time to exit.\n"
+  $redis.subscribe(:community) do |on|
+    on.message do |channel, msg|
+      data = JSON.parse(msg)
+      puts "[#{data['user']}]: #{data['msg']}"
+    end
   end
+rescue Interrupt => e
+  puts "\nGoodbye."
 end
--- talker.rb
+++ talker.rb
@@ -2,6 +2,14 @@ require './lib/redis-config'
 
 data = {user: ARGV[0] || ENV['USER']}
 
+puts <<END
+Welcome to Slacker
+==================
+
+(Type 'exit' to leave.)
+
+END
+
 loop do
   print '> '
   msg = STDIN.gets
